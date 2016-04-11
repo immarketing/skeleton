c0e6f682c1f7500414549413accf7f0d40d222db
immarketing <immarketing@mail.ru>
2016-04-11 22:44:08 +0300

    111

---
+++ t.u.cmd
@@ -0,0 +1,6 @@
+@rem tools update command file
+@rem lessc ./less/algo.less > algo.css
+@rem lessc ./node_modules/bootstrap/less/bootstrap.less > bootstrap.css
+cmd /C npm update
+cmd /C bower update
+cmd /C composer update
